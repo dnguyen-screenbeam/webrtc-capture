a50cd529da1235a1916cdbbdc7e82f6732152ff3
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-26 14:58:16 -0700

    Add send stream functionality.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
   };
 
   let stream = null;
+  let peers = {
+    local: null,
+    remote: null
+  };
 
   const getStream = async () => {
     try {
@@ -21,14 +25,66 @@
       throw new Error(`Unable to get stream ${err.stack}`);
     }
   };
+
+  const createRemotePeer = () => {
+    const peer = new RTCPeerConnection();
+    peer.onicecandidate = e => {
+      if (e.candidate) {
+        peers.local.addIceCandidate(e.candidate);
+      }
+    };
+
+    // display stream on remote video element
+    peer.ontrack = e => {
+      if (dom.video.remote.srcObject !== e.streams[0]) {
+        dom.video.remote.srcObject = e.streams[0];
+        dom.video.remote.play();
+      }
+    };
+    
+    return peer;
+  };
+
+  const createLocalPeer = () => {
+    const peer = new RTCPeerConnection();
+
+    // Add tracks to local peer connection
+    stream.getTracks().forEach(track => {
+      peer.addTrack(track, stream);
+    });
+
+    // Set up event handlers for the ICE negotiation
+    peer.onicecandidate = e => {
+      if (e.candidate) {
+        peers.remote.addIceCandidate(e.candidate);
+      }
+    };
+
+    return peer;
+  };
+
+  const sendStream = async () => {
+    peers.local = createLocalPeer();
+    peers.remote = createRemotePeer();
+
+    // Create Offer and set local description
+    const offer = await peers.local.createOffer();
+    await peers.local.setLocalDescription(offer);
+    await peers.remote.setRemoteDescription(offer);
+
+    // Create Answer and set remote description
+    const answer = await peers.remote.createAnswer();
+    await peers.remote.setLocalDescription(answer);
+    await peers.local.setRemoteDescription(answer);
+  };
 </script>
 
 <div class="h-full flex flex-col gap-5">
-  <button
-    on:click={getStream}
-    class="border-gray-500 border rounded-lg px-5 py-2 w-fit hover:bg-indigo-800 hover:text-gray-50"
-    >Share screen</button
-  >
+  <div class="flex gap-5">
+    <button on:click={getStream}>Display screen</button>
+
+    <button on:click={sendStream}>Send stream</button>
+  </div>
 
   <div class="flex gap-5 h-full">
     <div class="flex flex-col gap-3 h-1/2">
@@ -48,5 +104,9 @@
 <style lang="postcss">
   video {
     @apply border-gray-500 border-2;
+  }
+
+  button {
+    @apply border-gray-500 border rounded-lg px-5 py-2 w-fit hover:bg-indigo-800 hover:text-gray-50;
   }
 </style>
