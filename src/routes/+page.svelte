bc380b915f56d0c2087aabbc24fef22326971d69
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-26 14:35:24 -0700

    Setup tailwindcss.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,42 @@
-<h1>This is the outerpage</h1>
+<script>
+  let dom = {
+    video: {
+      local: null,
+      remote: null
+    }
+  };
+
+  let stream = null;
+
+  const getStream = async () => {
+    try {
+      stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true
+      });
+
+      dom.video.local.srcObject = stream;
+      dom.video.local.play();
+    } catch (err) {
+      throw new Error(`Unable to get stream ${err.stack}`);
+    }
+  };
+</script>
+
+<div class="h-full flex flex-col gap-5">
+  <button on:click={getStream} class="border-gray-500 border rounded-lg px-5 py-2 w-fit hover:bg-indigo-800 hover:text-gray-50">Share screen</button>
+
+  <div class="flex">
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <video bind:this={dom.video.local} />
+
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <video bind:this={dom.video.remote} />
+  </div>
+</div>
+
+<style lang="postcss">
+  video {
+    @apply border-gray-500 border-2;
+  }
+</style>
--- src/routes/innerpage/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>This is the innerpage</h1>
