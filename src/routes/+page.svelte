468e21dd3a03e3e878002183f773f494666d3206
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-26 14:41:13 -0700

    Style video.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,14 +24,24 @@
 </script>
 
 <div class="h-full flex flex-col gap-5">
-  <button on:click={getStream} class="border-gray-500 border rounded-lg px-5 py-2 w-fit hover:bg-indigo-800 hover:text-gray-50">Share screen</button>
+  <button
+    on:click={getStream}
+    class="border-gray-500 border rounded-lg px-5 py-2 w-fit hover:bg-indigo-800 hover:text-gray-50"
+    >Share screen</button
+  >
 
-  <div class="flex">
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <video bind:this={dom.video.local} />
+  <div class="flex gap-5 h-full">
+    <div class="flex flex-col gap-3 h-1/2">
+      <p class="text-lg font-semibold">Local</p>
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video bind:this={dom.video.local} class="h-full" />
+    </div>
 
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <video bind:this={dom.video.remote} />
+    <div class="flex flex-col gap-3 h-1/2">
+      <p class="text-lg font-semibold">Remote</p>
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video bind:this={dom.video.remote} class="h-full" />
+    </div>
   </div>
 </div>
 
